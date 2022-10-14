8b9de757ef2b721c5aa57750d62cc9fd49a19366
Hardik <hardik.sj@somaiya.edu>
2022-10-15 04:49:41 +0530

    fix: Readme updated

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Habit Tracker',
       home: const HomePage(),
       theme: ThemeData(
         primarySwatch: Colors.green,
